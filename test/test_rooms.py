2532c235fc2e07de31668f867b5c47f52c6acb8c
Julius <bassin.yoel@gmail.com>
2022-07-11 23:38:36 +0300

    add on receive and broadcast tests

--- test/test_rooms.py
+++ test/test_rooms.py
@@ -1,4 +1,7 @@
-from typing import Any
+import asyncio
+from ctypes import Union
+import logging
+from typing import Any, List
 from fastapi import Depends, FastAPI, WebSocket
 from websocket_rooms.room import Room
 import pytest
@@ -8,28 +11,192 @@ app = FastAPI()
 
 room = Room()
 
+
 @app.websocket("/ws_test")
 async def websocket_endpoint(websocket: WebSocket, room: Room = Depends(room)) -> None:
     await room.connect(websocket)
 
 
-@pytest.mark.asyncio
-async def test_on_receive():
-    test_room = Room()
-    app.dependency_overrides[room] = test_room
+@pytest.fixture
+def test_room():
+    _test_room = Room()
+    app.dependency_overrides[room] = _test_room
+    return _test_room
 
+
+@pytest.fixture(scope="function")
+async def test_client():
+    async with TestClient(app, timeout=0.1) as client:
+        yield client
+
+
+@pytest.mark.asyncio
+# thats pretty weird - needs further research
+@pytest.mark.parametrize(
+    "message", ["hi!", "1234", 1234, "\U0001f5ff", b"123", {"hello": "world"}]
+)
+async def test_on_receive(test_client: TestClient, test_room: Room, message: Any):
     message_buffer = []
 
     @test_room.on_receive()
-    def on_receive_test(room: Room, websocket: WebSocket, message: Any):
-        message_buffer.append(message)
+    def on_receive_test(room: Room, websocket: WebSocket, recv_message: Any):
+        message_buffer.append(recv_message)
 
-    print(test_room._on_receive)
+    async with test_client.websocket_connect("/ws_test") as ws:
+        await ws.send_text(message)
 
-    msg = "hi!"
+    await asyncio.sleep(0)
+    assert message_buffer == [message]
 
-    async with TestClient(app, timeout=0.1) as client:
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "message_type, message",
+    [("text", "abcd"), ("bytes", b"abcd"), ("json", {"message": "abcd"})],
+)
+async def test_on_receive_types(
+    test_client: TestClient,
+    test_room: Room,
+    message_type: Room.RECEIVE_TYPES,
+    message: Any,
+):
+    message_buffer = []
+
+    @test_room.on_receive(message_type)
+    def on_receive_test(room: Room, websocket: WebSocket, recv_message: Any):
+        message_buffer.append(recv_message)
+
+    async with test_client.websocket_connect("/ws_test") as ws:
+        await ws.__getattribute__(f"send_{message_type}")(message)
+
+    await asyncio.sleep(0)
+    assert message_buffer == [message]
+
+
+# thats pretty weird - needs further research
+@pytest.mark.asyncio
+@pytest.mark.parametrize("messages", [["hi!", "hello", "yes"]])
+async def test_multiple_on_receive(
+    test_client: TestClient, test_room: Room, messages: List[Any]
+):
+    message_buffer = []
+
+    @test_room.on_receive()
+    def on_receive_test(room: Room, websocket: WebSocket, recv_message: Any):
+        message_buffer.append(recv_message)
+
+    async with test_client.websocket_connect("/ws_test") as ws:
+        for message in messages:
+            await ws.send_text(message)
+
+    await asyncio.sleep(0)
+    assert message_buffer == messages
+
+
+# thats pretty weird - needs further research
+@pytest.mark.asyncio
+@pytest.mark.parametrize("messages", [["hi!", "hello", "yes"]])
+async def test_on_receive_one_after_another(
+    test_client: TestClient, test_room: Room, messages: List[Any]
+):
+    message_buffer = []
+
+    @test_room.on_receive()
+    def on_receive_test(room: Room, websocket: WebSocket, recv_message: Any):
+        message_buffer.append(recv_message)
+
+    for message in messages:
+        async with test_client.websocket_connect("/ws_test") as ws:
+            await ws.send_text(message)
+
+    await asyncio.sleep(0)
+    assert message_buffer == messages
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("message, client_count", [("hello", 10)])
+async def test_multiple_on_receive_simultaniously(
+    test_client: TestClient, test_room: Room, message: Any, client_count: int
+):
+    message_buffer = []
+
+    @test_room.on_receive()
+    def on_receive_test(room: Room, websocket: WebSocket, recv_message: Any):
+        message_buffer.append(recv_message)
+
+    async def websocket_client(client: TestClient, msg: Any):
         async with client.websocket_connect("/ws_test") as ws:
             await ws.send_text(msg)
-    assert message_buffer == [msg]
-    
+
+    messages = [f"{message}-{i}" for i in range(client_count)]
+
+    await asyncio.gather(*[websocket_client(test_client, msg) for msg in messages])
+
+    assert set(message_buffer) == set(messages)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "message", ["hi!", "1234", 1234, "\U0001f5ff", b"123", {"hello": "world"}]
+)
+async def test_broadcast(test_client: TestClient, test_room: Room, message: Any):
+    @test_room.on_receive()
+    async def on_receive_test(room: Room, websocket: WebSocket, message: Any):
+        await test_room.push_text(message)
+
+    async with test_client.websocket_connect("/ws_test") as ws_1:
+        async with test_client.websocket_connect("/ws_test") as ws_2:
+            await ws_1.send_text(message)
+            recv = await ws_2.receive_text()
+            assert recv == message
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "message_type, message",
+    [("json", {"message": "hello"}), ("text", "hello"), ("bytes", b"hello")],
+)
+async def test_types_broadcast(
+    test_client: TestClient,
+    test_room: Room,
+    message_type: Room.RECEIVE_TYPES,
+    message: Any,
+):
+    @test_room.on_receive(message_type)
+    async def on_receive_test(room: Room, websocket: WebSocket, message: Any):
+        await test_room.__getattribute__(f"push_{message_type}")(message)
+
+    async with test_client.websocket_connect("/ws_test") as ws_1:
+        async with test_client.websocket_connect("/ws_test") as ws_2:
+            await ws_1.__getattribute__(f"send_{message_type}")(message)
+            recv = await ws_2.__getattribute__(f"receive_{message_type}")()
+            assert recv == message
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "message", ["hi!", "1234", 1234, "\U0001f5ff", b"123", {"hello": "world"}]
+)
+async def test_multiple_broadcast(
+    test_client: TestClient,
+    test_room: Room,
+    message: Any,
+):
+    RECEIVERS_NUMBER = 10
+
+    @test_room.on_receive()
+    async def on_receive_test(room: Room, websocket: WebSocket, message: Any):
+        await test_room.push_text(message)
+
+    async def receiver():
+        async with test_client.websocket_connect("/ws_test") as ws:
+            recv = await ws.receive_text()
+            assert recv == message
+
+    async def sender():
+        # wait for all websockets to be created?
+        await asyncio.sleep(0.1)
+        async with test_client.websocket_connect("/ws_test") as ws:
+            await ws.send_text(message)
+
+    await asyncio.gather(*[receiver() for _ in range(RECEIVERS_NUMBER)], sender())
--- websocket_rooms/room.py
+++ websocket_rooms/room.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
+import asyncio
 from enum import Enum
 from gc import callbacks
 import inspect
 import json
+import logging
 from typing import Any, Callable, Dict, List, Union, NoReturn, get_args
 from starlette.websockets import WebSocketState, Message, WebSocket, WebSocketDisconnect
+
 
 class ReceiveType(Enum):
     TEXT = "text"
@@ -13,12 +16,15 @@ class ReceiveType(Enum):
 
 
 class Room:
-    RECEIVE_TYPES = Union[ReceiveType.TEXT.value, ReceiveType.JSON.value, ReceiveType.BYTES.value
+    RECEIVE_TYPES = Union[
+        ReceiveType.TEXT.value, ReceiveType.JSON.value, ReceiveType.BYTES.value
     ]
 
     def __init__(self):
         self._websockets: List[WebSocket] = []
-        self._on_receive: Dict[self.RECEIVE_TYPES, Callable[[Room, WebSocket], None]] = {}
+        self._on_receive: Dict[
+            self.RECEIVE_TYPES, Callable[[Room, WebSocket], None]
+        ] = {}
 
     async def connect(self, websocket: WebSocket) -> NoReturn:
         await websocket.accept()
@@ -37,62 +43,77 @@ class Room:
         living_connections: List[WebSocket] = []
         while len(self._websockets) > 0:
             websocket = self._websockets.pop()
-            await websocket.send_
+            await websocket.send_json(message)
             living_connections.append(websocket)
         self._websockets = living_connections
 
     async def push_text(self, message: str) -> None:
-        pass
+        living_connections: List[WebSocket] = []
+        while len(self._websockets) > 0:
+            websocket = self._websockets.pop()
+            await websocket.send_text(message)
+            living_connections.append(websocket)
+        self._websockets = living_connections
 
     async def push_bytes(self, message: bytes) -> None:
-        pass
+        living_connections: List[WebSocket] = []
+        while len(self._websockets) > 0:
+            websocket = self._websockets.pop()
+            await websocket.send_bytes(message)
+            living_connections.append(websocket)
+        self._websockets = living_connections
 
-    async def _run_client_lifecycle(self, websocket: WebSocket) -> None:
+    async def _run_client_lifecycle(self, websocket: WebSocket) -> NoReturn:
         try:
-            if websocket.application_state != WebSocketState.CONNECTED:
-                raise RuntimeError(
-                    'WebSocket is not connected. Need to call "accept" first.'
-                )
-            message: Message = await websocket.receive()
-            websocket._raise_on_disconnect(message)
+            while True:
+                if websocket.application_state != WebSocketState.CONNECTED:
+                    raise RuntimeError(
+                        'WebSocket is not connected. Need to call "accept" first.'
+                    )
+                message: Message = await websocket.receive()
+                logging.info(message)
+                websocket._raise_on_disconnect(message)
 
-            func = None
-            text = None
+                func = None
+                text = None
 
-            if ReceiveType.BYTES.value in message.keys():
-                text = message[ReceiveType.BYTES.value].decode("utf-8")
-                if ReceiveType.BYTES.value in self._on_receive.keys():
-                    func = self._on_receive[ReceiveType.BYTES.value]
-                elif ReceiveType.JSON in self._on_receive.keys():
-                    text = json.loads(text)
-                    func = self._on_receive[ReceiveType.JSON.value]
-            elif ReceiveType.TEXT.value in message.keys():
-                text = message[ReceiveType.TEXT.value]
-                if ReceiveType.TEXT.value in self._on_receive.keys():
-                    func = self._on_receive[ReceiveType.TEXT.value]
-                elif ReceiveType.JSON in self._on_receive.keys():
-                    text = json.loads(text)
-                    func = self._on_receive[ReceiveType.JSON.value]
+                if ReceiveType.BYTES.value in message.keys():
+                    text = message[ReceiveType.BYTES.value]
+                    if ReceiveType.BYTES.value in self._on_receive.keys():
+                        func = self._on_receive[ReceiveType.BYTES.value]
+                    elif ReceiveType.JSON in self._on_receive.keys():
+                        text = json.loads(text.decode("utf-8"))
+                        func = self._on_receive[ReceiveType.JSON.value]
+                elif ReceiveType.TEXT.value in message.keys():
+                    text = message[ReceiveType.TEXT.value]
+                    if ReceiveType.TEXT.value in self._on_receive.keys():
+                        func = self._on_receive[ReceiveType.TEXT.value]
+                    elif ReceiveType.JSON.value in self._on_receive.keys():
+                        text = json.loads(text)
+                        func = self._on_receive[ReceiveType.JSON.value]
 
-            if func and text:
-                func_res = func(self, websocket, text)
-                if inspect.isawaitable(func_res):
-                    await func_res
+                if func and text:
+                    func_res = func(self, websocket, text)
+                    if inspect.isawaitable(func_res):
+                        await func_res
 
         except WebSocketDisconnect:
             await self.remove(websocket)
 
     def on_receive(self, mode: Room.RECEIVE_TYPES = ReceiveType.TEXT.value) -> callable:
         if not mode in ["text", "bytes", "json"]:
-            raise RuntimeError('The "mode" argument should be "text", "bytes" or "json".')
+            raise RuntimeError(
+                'The "mode" argument should be "text", "bytes" or "json".'
+            )
+
         def inner(func: Callable[[Room, WebSocket, Any], None]):
             self._on_receive[mode] = func
+            return func
 
         return inner
 
-    # def on_receive(self, func: Callable[[Room, WebSocket, Any]]):
-    #     self._on_receive["text"] = func
-    #     print(self._on_receive)
+
+    
 
     def __call__(self) -> Room:
         return self
