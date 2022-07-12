63160efe7765f5a585e2a6d6d2788cf0f7160d24
Julius <bassin.yoel@gmail.com>
2022-07-12 23:12:18 +0300

    alpha version

--- setup.py
+++ setup.py
@@ -3,33 +3,27 @@ import pathlib
 
 topdir = pathlib.Path(__file__).parent
 
+
 def readfile(f):
     return (topdir / f).read_text("utf-8").strip()
 
+
 setuptools.setup(
     name="websocket-rooms",
-    version="0.0.1",
+    version="0.1.0",
     description="Python library for simple websocket managment",
-    # long_description=readfile("README.md"),
+    long_description=readfile("README.md"),
     url="https://github.com/yoelbassin/Websocket-Rooms",
     author="Yoel Bassin",
     author_email="bassin.yoel@gmail.com",
     license="MIT",
     classifiers=[
-        "Development Status :: 1 - Planning"
-        "Intended Audience :: Developers",
+        "Development Status :: 2 - Pre-Alpha" "Intended Audience :: Developers",
         "Framework :: AsyncIO, Starlette",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
     ],
-    # package_data={"websocket-rooms": ["*.pyi", "py.typed"]},
-    # include_package_data=True,
     packages=["websocket_rooms"],
-    python_requires=">=3.5",
-    install_requires=[
-        "starlette"
-    ],
-    extras_require={"test": [
-        "fastapi", "pytest", "pytest-asyncio"
-    ]},
+    install_requires=["starlette"],
+    extras_require={"test": ["fastapi", "pytest", "pytest-asyncio"], "example": ["fastapi", "uvicorn", "websockets"]},
 )
